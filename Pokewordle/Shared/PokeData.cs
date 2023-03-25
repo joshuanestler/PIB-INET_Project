a0841f5aaff3a4d9a99b1e96828cb57d6d3a1047
Joshua Nestler <joshua.nestler@mail.de>
2023-03-25 16:24:07 +0100

    Modified code

--- Pokewordle/Shared/PokeData.cs
+++ Pokewordle/Shared/PokeData.cs
@@ -4,32 +4,32 @@ namespace Pokewordle.Shared
 {
     public class PokeData
     {
-        public string Name { get; set; }
-        
-        public string Type1 { get; set; }
-        public Color Type1Color { get; set; }
-        public string Type1ColorStr { get => ConvertToHex(Type1Color); }
+        public readonly string Name;
 
-        public string? Type2 { get; set; }
-        public Color? Type2Color { get; set; }
-        public string Type2ColorStr { get => Type1Color.ToString(); }
+        public readonly string Type1;
+        public readonly Color Type1Color;
+        public string Type1ColorStr => ConvertToHex(Type1Color);
+
+        public readonly string? Type2;
+        public readonly Color? Type2Color;
+        public string Type2ColorStr => ConvertToHex(Type2Color);
 
         public int Generation { get; set; }
 
-        public int Height_m { get; set; }
+        public readonly int Height_m;
         public int Weight_kg { get; set; }
         //public int EvolutionType { get; set; }
 
         public List<string> Abilities { get; set; }
 
-        private static String ConvertToHex(System.Drawing.Color? c)
+        private static string ConvertToHex(System.Drawing.Color? c)
         {
-            if (c is null)
-            {
+            if (c is null) {
                 return "#AAAAAA";
             }
-            return "#" + c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2");
+            
+            Color color = (Color)c;
+            return "#" + color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
         }
-
     }
 }
