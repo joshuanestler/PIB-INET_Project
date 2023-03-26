54a1590bb9ae269d9ec81250617f251d6db30d64
Tobias Kronser <>
2023-03-26 15:37:31 +0200

    Include razor templates

--- Pokewordle/Shared/HtmlUtil/SimpleTableCell.cs
+++ Pokewordle/Shared/HtmlUtil/SimpleTableCell.cs
@@ -5,11 +5,11 @@ namespace Pokewordle.Shared.HtmlUtil
 {
     public readonly record struct SimpleTableCell : ITableCell
     {
-        private readonly string DisplayString;
-        private readonly string FontColor;
-        private readonly string Background;
-        private readonly string HtmlClass;
-        private readonly string HtmlId;
+        public readonly string DisplayString;
+        public readonly string FontColor;
+        public readonly string Background;
+        public readonly string HtmlClass;
+        public readonly string HtmlId;
 
         public SimpleTableCell(string displayString = "", Color? background = null, Color? fontColor = null,
             string htmlClass = "", string htmlId = "")
@@ -37,11 +37,8 @@ namespace Pokewordle.Shared.HtmlUtil
             sb.Append($"style = \"background-color: {Background};\"");
             sb.Append(">\n");
 
-            //sb.Append($"<pre style = \"background-color: {Background};\">");
-
             sb.Append(DisplayString);
 
-            //sb.Append("</pre>\n");
             sb.Append("</td>");
 
             return sb.ToString();
