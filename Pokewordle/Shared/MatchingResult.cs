a8db42f91c549a6d683a632e699ce6f61f7aa93f
Tobias Kronser <>
2023-03-27 13:31:00 +0200

    Update MatchingResult, Add Extension Method

--- Pokewordle/Shared/MatchingResult.cs
+++ Pokewordle/Shared/MatchingResult.cs
@@ -1,9 +1,37 @@
-﻿namespace Pokewordle.Shared
+﻿using System.Drawing;
+
+namespace Pokewordle.Shared
 {
     public enum MatchingResult
     {
         ALL,
         PARTIAL,
+        PARTIAL_1_OF_2,
+        PARTIAL_1_OF_3,
+        PARTIAL_2_OF_3,
         NONE,
+    }
+
+    public static class MatchingResultExtensions
+    {
+        public static Color ToTruePartialFalseColor(this MatchingResult matchingResult)
+        {
+            return matchingResult switch
+            {
+                MatchingResult.NONE => ColorScheme.COLOR_MISTAKE,
+                MatchingResult.ALL => ColorScheme.COLOR_CORRECT,
+                _ => ColorScheme.COLOR_SEMI_CORRECT_MISTAKE
+            };
+        }
+
+        public static Color ToBinaryColor(this MatchingResult matchingResult)
+        {
+            if (matchingResult == MatchingResult.ALL)
+            {
+                return ColorScheme.COLOR_CORRECT;
+            }
+            return ColorScheme.COLOR_MISTAKE;
+        }
+
     }
 }
