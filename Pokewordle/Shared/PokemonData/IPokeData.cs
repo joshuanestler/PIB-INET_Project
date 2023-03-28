0099f635b2819d1ed7630a7a87dc8c8a56cb495f
Tobias Kronser <>
2023-03-28 16:07:13 +0200

    Various Additions

--- Pokewordle/Shared/PokemonData/IPokeData.cs
+++ Pokewordle/Shared/PokemonData/IPokeData.cs
@@ -12,13 +12,21 @@ namespace Pokewordle.Shared.PokemonData
         //ALWAYS CONTAINS AT LEAST 2 VALUES
         IImmutableList<string> FilledTypes { get; }
 
-        //Generation: Pokemon -> Species -> Generation
-
         float Height_m { get; }
         float Weight_kg { get; }
         //public int EvolutionType { get; set; }
+        string SpriteUrl { get; }
 
         IImmutableList<string> Abilities { get; }
+
+        int HP { get; }
+        int Atk { get; }
+        int Def { get; }
+        int SpA { get; }
+        int SpD { get; }
+        int Spe { get; }
+
+
 
         Task<int> GetGeneration();
     }
