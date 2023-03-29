35af0476a664f259064017a1c96f616229a1031d
Tobias Kronser <>
2023-03-29 22:13:00 +0200

    Add Example Settings Functionality

---
+++ Pokewordle/Shared/Util/SharedSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Pokewordle.Shared.Util
+{
+    public static class SharedSettings
+    {
+        public static List<ColumnType> GameColumnTypes = new List<ColumnType>() { ColumnType.SPRITE, ColumnType.NAME, ColumnType.GENERATION, ColumnType.TYPES, ColumnType.ABILITIES, ColumnType.HEIGHT, ColumnType.WEIGHT };
+    }
+}
--- Pokewordle/Shared/Util/Translations.cs
+++ Pokewordle/Shared/Util/Translations.cs
@@ -1,17 +1,32 @@
-﻿using System.Collections.Concurrent;
+﻿using Microsoft.VisualBasic.FileIO;
+using System.Collections.Concurrent;
 
 namespace Pokewordle.Shared.Util
 {
     public static class Translations
     {
-        private static readonly ConcurrentDictionary<string, string> s_PokemonNameTranslated;
+        private const string POKEMON_NAMES_FILE = @"";
+        private const string CSV_DELIMITERS = ",";
+        private static readonly ConcurrentDictionary<string, string> s_PokemonNameTranslated = new();
 
-        public static void LoadLanguage()
+        public static void LoadLanguage(int languageColumn)
         {
             lock(s_PokemonNameTranslated)
             {
                 s_PokemonNameTranslated.Clear();
                 //Add names based on column in csv file
+                using TextFieldParser parser = new(POKEMON_NAMES_FILE);
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(CSV_DELIMITERS);
+                while (!parser.EndOfData)
+                {
+                    //Processing row
+                    if (parser.ReadFields() is string[] fields)
+                    {
+                        s_PokemonNameTranslated[fields[0]] = fields[languageColumn];
+                    }
+                }
+
             }
         }
 
