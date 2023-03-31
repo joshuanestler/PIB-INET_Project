e556bb7c8bfb35b334d339b991200804a01516c9
Joshua Nestler <joshua.nestler@mail.de>
2023-03-31 20:30:21 +0200

    Settings description & revert to old layout

--- Pokewordle/Shared/Util/Theme.cs
+++ Pokewordle/Shared/Util/Theme.cs
@@ -2,7 +2,7 @@ namespace Pokewordle.Shared.Util;
 
 public enum Theme
 {
-    Forest, Vaporwave, Beach, Melon, Ocean, Sand, Peach, Hal9000
+     Vaporwave, Beach, Melon, Ocean, Sand, Peach, Hal9000, Forest
 }
 
 public static class ThemeUtils
