e070589a5d8e52913ab007f0599e3067c4c08bc2
Joshua Nestler <joshua.nestler@mail.de>
2023-03-31 16:18:11 +0200

    Added saved settings

--- Pokewordle/Program.cs
+++ Pokewordle/Program.cs
@@ -23,16 +23,14 @@ builder.Services.AddFluentUIComponents();
 
 WebAssemblyHost host = builder.Build();
 
-SettingsService settingsService = host.Services.GetService<SettingsService>();
+SettingsService settingsService = host.Services?.GetService<SettingsService>()!;
 
-if (settingsService is null) {
-    Console.WriteLine("Settings service is null!");
-}
-
+// Retrieve saved theme setting from local storage
 Enum.TryParse(await settingsService.GetSettingAsync("theme"), out Theme theme);
 Console.WriteLine("Theme: " + theme);
 SharedSettings.SelectedTheme = theme;
 
+// Retrieve saved language setting from local storage
 string language = await settingsService.GetSettingAsync("language");
 if (!SharedSettings.Languages.Contains(language)) {
     language = SharedSettings.Languages[0];
@@ -40,7 +38,29 @@ if (!SharedSettings.Languages.Contains(language)) {
 SharedSettings.SelectedLanguage = language;
 int index = SharedSettings.Languages.IndexOf(language);
 
+// Retrieve saved obfuscation setting from local storage
+bool tryParse = int.TryParse(await settingsService.GetSettingAsync("obfuscation"), out int obfuscate);
+if (!tryParse) {
+    obfuscate = 0;
+}
+SharedSettings.Obfuscation = obfuscate;
 
+// Retrieve saved guess_history_limit setting from local storage
+tryParse = int.TryParse(await settingsService.GetSettingAsync("guess_history_limit"), out int guessHistoryLimit);
+if (!tryParse) {
+    guessHistoryLimit = 0;
+}
+SharedSettings.GuessHistoryLimit = guessHistoryLimit;
+
+// Retrieve saved allow_duplicate_guesses setting from local storage
+tryParse = bool.TryParse(await settingsService.GetSettingAsync("allow_duplicate_guesses"), out bool allowDuplicateGuesses);
+if (!tryParse) {
+    allowDuplicateGuesses = false;
+}
+SharedSettings.AllowDuplicateGuesses = allowDuplicateGuesses;
+
+// Initialize translations
 await Translations.Initialize(httpClient, pokeApiClient,index + 1);
 
+// Start application
 await host.RunAsync();
