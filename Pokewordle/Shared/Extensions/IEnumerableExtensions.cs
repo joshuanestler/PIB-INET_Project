477daca8a20a18b8a28f1338d1f895d324221275
Tobias Kronser <>
2023-04-01 16:16:58 +0200

    Basic Enumerable Extensions

---
+++ Pokewordle/Shared/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Pokewordle.Shared.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> items, Action<T> action)
+        {
+            foreach(T item in items)
+            {
+                action.Invoke(item);
+            }
+        }
+
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> items, Predicate<T> filter)
+        {
+            foreach (T item in items)
+            {
+                if (filter.Invoke(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+
+    }
+}
